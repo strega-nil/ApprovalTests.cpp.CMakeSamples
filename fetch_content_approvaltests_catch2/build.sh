7c308f6eebc1892b7dcbaeb5fea10bbc3f963dd3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-19 16:48:49 +0000

    Check for errors in script

--- fetch_content_approvaltests_catch2/build.sh
+++ fetch_content_approvaltests_catch2/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Force execution to halt if there are any errors in this script:
+set -e
+set -o pipefail
+
 sourcedir=`pwd`
 mkdir -p cmake-build-spaces/cmake-build-command-line
 cd       cmake-build-spaces/cmake-build-command-line
