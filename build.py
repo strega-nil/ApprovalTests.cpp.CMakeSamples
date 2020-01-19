15267456aed4371d73b13ae1ee113fc3455039d9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-19 16:53:57 +0000

    Add Python script to build all sub-directories

---
+++ build.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+ignored_paths = ['.git', '.idea']
+
+for dir in os.listdir(os.getcwd()):
+    if not os.path.isdir(dir):
+        continue
+    if os.path.islink(dir):
+        continue
+    if dir in ignored_paths:
+        continue
+    print('==============================', dir)
+    os.chdir(dir)
+
+    success = True
+    try:
+        output = subprocess.check_output(['./build.sh'])
+    except subprocess.CalledProcessError as e:
+        output = e.output
+        success = False
+
+    print(output)
+    if not success:
+        exit(1)
+
+    os.chdir('..')
