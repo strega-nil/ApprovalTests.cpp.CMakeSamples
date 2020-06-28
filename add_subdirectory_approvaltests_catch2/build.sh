ad4ac40365ce2260b83ef2d856b7107c335d98f8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-28 16:27:49 +0100

    e   Simplify build scripts

--- add_subdirectory_approvaltests_catch2/build.sh
+++ add_subdirectory_approvaltests_catch2/build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Force execution to halt if there are any errors in this script:
-set -e
-set -o pipefail
-
 sourcedir=`pwd`
 mkdir -p cmake-build-spaces/cmake-build-command-line
 cd       cmake-build-spaces/cmake-build-command-line
