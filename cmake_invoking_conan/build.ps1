fabf305e1b67dfcdd8fa8ccf4388e08916ee0296
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-01-07 23:04:03 +0000

    Try adding a windows build script

---
+++ cmake_invoking_conan/build.ps1
@@ -0,0 +1,10 @@
+mkdir  -force build
+Push-Location build
+
+# Note that we do not need to invoke conan.
+# However, we do need to say what build configuration we want.
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build .
+ctest --output-on-failure .
+
+Pop-Location
